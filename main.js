fb6016c32bc201f864c1f4f48960edaa2959b352
paarthd00 <paarthcode@gmail.com>
2023-09-06 12:42:56 -0700

    @fix check for directory

--- main.js
+++ main.js
@@ -1,19 +1,13 @@
 import { BlobServiceClient, StorageSharedKeyCredential } from "@azure/storage-blob";
-
 import { join } from "path";
-
 import fs from 'fs';
-
 import dotenv from 'dotenv';
 
 dotenv.config()
 
 const fsPromises = fs.promises;
-
 const account = process.env.ACCOUNT_NAME;
-
 const accountKey = process.env.ACCOUNT_KEY;
-
 const sharedKeyCredential = new StorageSharedKeyCredential(account, accountKey);
 
 const blobServiceClient = new BlobServiceClient(
@@ -22,13 +16,10 @@ const blobServiceClient = new BlobServiceClient(
 );
 
 const containerName = process.env.CONTAINER_NAME;
-
 const directoryName = "backup"
 
 async function main() {
-
     const containerClient = blobServiceClient.getContainerClient(containerName);
-
     const blobs = containerClient.listBlobsFlat();
 
     for await (const blob of blobs) {
@@ -36,20 +27,15 @@ async function main() {
         await blobClient.downloadToFile(join(directoryName, blob.name));
         console.log(`Downloaded blob "${blob.name}" successfully`);
     }
-
 }
 
 if (!fs.existsSync(directoryName)) {
-
     fsPromises.mkdir(directoryName).then(() => {
         main();
     }).catch((err) => {
         throw new Error(`Failed to create backup`);
     });
-
 } else {
-
     main();
-
 }
 
